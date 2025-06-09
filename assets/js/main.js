d50c7ad7e1710db6c255e8fcec0f3165a6c0d2f1
Mavlon4658 <mavlonbe4658>
2025-06-09 10:10:39 +0500

    home desktop is ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,3 +42,21 @@ const popularSwp = new Swiper('.popular-swp .swiper', {
         }
     }
 })
+
+const homeSwp = new Swiper('.home-swp .swiper', {
+    slidesPerView: 1,
+    loop: true,
+    speed: 800,
+    navigation: {
+        nextEl: '.home-bottom__right .next-btn',
+        prevEl: '.home-bottom__right .prev-btn',
+    }
+})
+
+if (homeSwp) {
+    const swpFraction = document.querySelector('.home-bottom__right .swp-fraction');
+    swpFraction.innerHTML = `<span>${homeSwp.realIndex < 9 ? '0' +( homeSwp.realIndex+1) : ( homeSwp.realIndex+1)} /</span> ${homeSwp.slides.length < 9 ? '0' + homeSwp.slides.length : homeSwp.slides.lengt}`
+    homeSwp.on('slideChange', function (e) {
+        swpFraction.innerHTML = `<span>${homeSwp.realIndex < 9 ? '0' +( homeSwp.realIndex+1) : ( homeSwp.realIndex+1)} /</span> ${homeSwp.slides.length < 9 ? '0' + homeSwp.slides.length : homeSwp.slides.lengt}`
+    });
+}
